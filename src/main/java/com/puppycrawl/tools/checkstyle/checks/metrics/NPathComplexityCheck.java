578885cce02b76e79e8b6241d321aff61a5d3600
rnveach <rveach02@gmail.com>
2023-01-13 23:59:52 -0500

    test 9

--- src/main/java/com/puppycrawl/tools/checkstyle/checks/metrics/NPathComplexityCheck.java
+++ src/main/java/com/puppycrawl/tools/checkstyle/checks/metrics/NPathComplexityCheck.java
@@ -515,7 +515,8 @@ public final class NPathComplexityCheck extends AbstractCheck {
 
     /** Leaves catch. */
     private void leaveAddingConditional() {
-        currentRangeValue = currentRangeValue.add(popValue().getRangeValue().add(BigInteger.ONE));
+        popValue();
+        currentRangeValue = /*currentRangeValue.add(popValue().getRangeValue().add(*/BigInteger.ONE;//));
     }
 
     /**
